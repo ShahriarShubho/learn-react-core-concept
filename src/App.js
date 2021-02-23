3cd2185b8efc8bbfafdbf82e84ec20b30c1edd63
Shahriar Shubho <you@example.com>
2021-02-23 13:22:57 +0600

    daynamic component add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name : "shubho"
+  }
+
+  const product = [{
+    name: "PhotoShope",
+    price : "$200",
+  },
+  { name: "Pdf reader",
+  price : "$100",
+}
+]
+ 
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +22,34 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>My name is {person.name}</h3>
+        <p style = {{  color : "green"}}>This is shubho</p>
+        <Products product = {product[0]}></Products>
+        <Products product = {product[1]}></Products>
+
       </header>
     </div>
   );
+}
+
+function Products(props){
+  const productStyle ={
+    width : '400px',
+    height : '400px',
+    backgroundColor : "lightgrey",
+    margin : '10px',
+    color: "black",
+  }
+ const {name , price} = props.product;
+  return(
+    <div style = {productStyle}>
+      <h4>Name : {name} </h4>
+      <h5>Price : {price} </h5>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+      <button>Buy Now</button>
+    </div>
+  )
 }
 
 export default App;
